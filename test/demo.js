ccbfa03aae3623db25993248cf590c17bfc08381
Naveen Ramesh <naveen_r@blr.trigent.com>
2020-10-15 12:00:47 +0530

    sfd

--- test/demo.js
+++ test/demo.js
@@ -9,7 +9,7 @@ describe("Real Appeal Email Support Webform", function(){
         var title = "abd"
         if(title ==="abd")
         {
-            console.log("success") //sdfdsf
+            console.log("successful") //sdfdsf
         }        
         else{
             console.log("failure")
