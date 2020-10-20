0622f0ba998893059166239fbd2fe3d4d86650af
Naveen Ramesh <naveen_r@blr.trigent.com>
2020-10-20 10:49:13 +0530

    changed pauses

--- test/demo.js
+++ test/demo.js
@@ -2,7 +2,7 @@ describe("Real Appeal Email Support Webform", function(){
 
     it("Login to SF later Launch and Submit Webform ",function(){
         browser.url('https://stackoverflow.com/questions/37055507/webdriverio-take-full-page-screenshot') 
-        browser.pause(10000)
+        browser.pause(20000)
         /*
         $('//title[contains(text(),"Login | Salesforce")]').waitForExist({timeout: 30000})
         var SFTitle = $('//title[contains(text(),"Login | Salesforce")]').getText()
